e2d4b12161527df8be817e724d6cbf4b770ecfb4
Jordan Gibson <jordangibson737@gmail.com>
2018-11-19 09:42:06 +0000

    Add project files.

---
+++ ConsoleExtensions/Console.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExtensions
+{
+    public static class Console
+    {
+        private static Random random = new Random();
+        private static string chars = "abcdefghijklmnopqrstuvwxyz";
+
+        public static ConsoleColor BackgroundColor { get { return System.Console.BackgroundColor; }
+            set { System.Console.BackgroundColor = value; } }
+
+        public static ConsoleColor ForegroundColor
+        {
+            get { return System.Console.ForegroundColor; }
+            set { System.Console.ForegroundColor = value; }
+        }
+
+        public static void WriteTable<T>(List<T> list)
+        {
+            string header = " ║ Index     ║ Value";
+            int count = header.Length + list.Max(s => s.ToString().Length);
+            System.Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine(" ╔".PadRight(13, '═') + "╦".PadRight(count - 8, '═') + "╗" + Environment.NewLine + header.PadRight(count + 5) + "║");
+            for(int i = 0; i < list.Count; i++)
+            {
+                string value = " " + i + "║ ".PadLeft(12 - i.ToString().Length) + list[i].ToString();
+                System.Console.Write(" ║");
+                System.Console.BackgroundColor = i % 2 == 0 ? ConsoleColor.DarkGray : ConsoleColor.Black;
+                System.Console.Write(value + "".PadRight(count - value.Length + 3));
+                System.Console.BackgroundColor = ConsoleColor.Black;
+                System.Console.WriteLine("║");
+            }
+            System.Console.WriteLine(" ╚" + "".PadRight(11, '═') + "╩" + "".PadRight(count - 9, '═') + "╝");
+        }
+
+        public static void RealtimeType(string text, int milliDelay)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (random.Next(0, 10) == 3)
+                {
+                    System.Console.Write(chars[random.Next(0, chars.Length)]);
+                    System.Threading.Thread.Sleep(milliDelay + (random.Next(0, milliDelay * 2) * 2));
+                    System.Console.Write("\b");
+                    i--;
+                }
+                else
+                {
+                    System.Console.Write(text[i]);
+                    System.Threading.Thread.Sleep(milliDelay + (random.Next(0, milliDelay * 2) - milliDelay));
+                }
+            }
+            System.Console.WriteLine();
+
+        }
+
+        public static int Read()
+        {
+            return System.Console.Read();
+        }
+
+        public static ConsoleKeyInfo ReadKey()
+        {
+            return System.Console.ReadKey();
+        }
+
+        public static string ReadLine()
+        {
+            return System.Console.ReadLine();
+        }
+
+        public static void Clear()
+        {
+            System.Console.Clear();
+        }
+
+        public static void Write(string text)
+        {
+            System.Console.Write(text);
+        }
+
+        public static void WriteLine(string text)
+        {
+            System.Console.WriteLine(text);
+        }
+    }
+}
---
+++ ConsoleExtensions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> chars = new List<int>();
+            List<string> strings = new List<string>();
+            Random random = new Random();
+            for(int i = 0; i < 8; i++)
+            {
+                chars.Add(random.Next());
+            }
+            for(int i = 0; i < 12; i++)
+            {
+                strings.Add(Guid.NewGuid().ToString("n").Substring(0, random.Next(6, 22)));
+            }
+            strings.Add("This is a test of a longer string");
+            Console.WriteTable(chars);
+            Console.WriteTable(strings);
+            Console.RealtimeType("This is a really cool way to do realtime typing", 50);
+            Console.ReadLine();
+        }
+    }
+}
