59b99ac431ef80883f71f1a2cf803012004f9d22
Jordan Gibson <jordangibson737@gmail.com>
2018-11-20 20:56:31 +0000

    Addition of ConsoleTable.cs

--- ConsoleExtensions/Console.cs
+++ ConsoleExtensions/Console.cs
@@ -30,109 +30,31 @@ namespace ConsoleExtensions
             get { return System.Console.ForegroundColor; }
             set { System.Console.ForegroundColor = value; }
         }
-
-        private static int padLeft = 0;
-        private static int padRight = 0;
-
-        //A line should be of the format "Key ║ Value"
-        private static void WriteLineInTable(string key, string[] lines, ConsoleColor currentColor)
-        {
-            for(int i = 0; i < lines.Length; i++)
-            {
-                System.Console.Write(" ║");
-                System.Console.BackgroundColor = currentColor;
-                if (i == 0)
-                {
-                    System.Console.Write(" " + key + " ║ ".PadLeft(padLeft - key.Length) + lines[i].PadRight(padRight - 1));
-                }
-                else
-                {
-                    System.Console.Write("║".PadLeft(padLeft) + lines[i].PadRight(padRight));
-                }
-                System.Console.BackgroundColor = ConsoleColor.Black;
-                System.Console.WriteLine("║");
-            }
-        }
-
-        private static void WriteTableValue(string key, string value, int i)
-        {
-            ConsoleColor currentColor = i % 2 == 0 ? ConsoleColor.DarkGray : ConsoleColor.Black;
-            string[] lines = value.Split('∞');
-            WriteLineInTable(key, lines, currentColor);
-        }
         
-        private static void WriteTable<T, U>(List<Tuple<T, U>> list, string[] headers = null)
+        private static void WriteTable<T, U>(IList<Tuple<T, U>> list, string[] headers = null)
         {
-            if (headers == null)
-                headers = new string[] { "Index", "Value" };
+            ConsoleTable consoleTable = new ConsoleTable(headers);
+            consoleTable.WriteTable(list);
+        }
 
-            padLeft = Math.Max(
-                        list.Max(x => x.Item1.ToString().Length),
-                        headers[0].Length)
-                        + 3;
-
-            padRight = Math.Min(Math.Max(
-                        list.Max(x => x.Item2.ToString().Length),
-                        headers[1].Length)
-                        + 2, 50);
-
-            string header = headers.Aggregate((a, b) => " ║ " + a + " ║ " + b + " ") +
-                "".PadRight(padRight - headers[1].Length - 2) + "║";
-
-            int maxWidth = padRight + padLeft + 10 > 50 ? 50 : padRight + padLeft + 10;
-
-            string topper = " ╔" + "╦".PadLeft(padLeft, '═') + "".PadRight(padRight, '═') + "╗" + Environment.NewLine + header;
-            string footer = " ╚" + "╩".PadLeft(padLeft, '═') + "".PadRight(padRight, '═') + "╝";
-
-            System.Console.ForegroundColor = ConsoleColor.White;
-            System.Console.WriteLine(topper);
-
+        public static void WriteTable<T>(IList<T> list)
+        {
+            List<Tuple<int, T>> tupleList = new List<Tuple<int, T>>();
             for(int i = 0; i < list.Count; i++)
             {
-                string[] values = list[i].Item2.ToString().Split(' ');
-                int characterCount = 0;
-                for (int j = 0; j < values.Length; j++)
-                {
-                    characterCount += values[j].Length;
-                    if (characterCount >= 40 && !(values[0].Length > 40))
-                    {
-                        var tempList = values.ToList();
-                        tempList.Insert(j, "∞");
-                        values = tempList.ToArray();
-                        characterCount = 0;
-                    }
-                }
-                if (values[0].Length > 48)
-                {
-                    for (int j = 48; j < values[0].Length; j += 48)
-                    {
-                        var temp = values[0];
-                        temp = temp.Insert(j, "∞ ");
-                        values = new string[] { temp };
-                    }
-                }
-                string value = values.Aggregate((a, b) => a + " " + b);
-                WriteTableValue(list[i].Item1.ToString(), value, i);
+                tupleList.Add(new Tuple<int, T>(i, list[i]));
             }
-            System.Console.WriteLine(footer);
+            WriteTable<int, T>(tupleList);
         }
 
-        public static void WriteTable<T>(List<T> list)
+        public static void WriteTable<T>(IList<T> list, string header1, string header2)
         {
-            List<Tuple<int, T>> keyValuePairList = new List<Tuple<int, T>>();
-            list.ForEach(x => {
-                keyValuePairList.Add(new Tuple<int, T>(list.IndexOf(x), x));
-            });
-            WriteTable<int, T>(keyValuePairList);
-        }
-
-        public static void WriteTable<T>(List<T> list, string header1, string header2)
-        {
-            List<Tuple<int, T>> keyValuePairList = new List<Tuple<int, T>>();
-            list.ForEach(x => {
-                keyValuePairList.Add(new Tuple<int, T>(list.IndexOf(x), x));
-            });
-            WriteTable(keyValuePairList, new string[] { header1, header2 });
+            List<Tuple<int, T>> tupleList = new List<Tuple<int, T>>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                tupleList.Add(new Tuple<int, T>(i, list[i]));
+            }
+            WriteTable(tupleList, new string[] { header1, header2 });
         }
 
         public static void RealtimeType(string text, int milliDelay)
---
+++ ConsoleExtensions/ConsoleTable.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExtensions
+{
+    public static class ConsoleTable
+    {
+        private static string[] headers;
+        private static int padLeft = 0;
+        private static int padRight = 0;
+
+        private static ConsoleColor currentColor {
+            get { return System.Console.BackgroundColor; }
+            set { System.Console.BackgroundColor = value; }
+        }
+
+        private static void WriteTableLine(string key, string[] lines)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                System.Console.Write(" ║");
+                System.Console.BackgroundColor = currentColor;
+                if (i == 0)
+                {
+                    System.Console.Write(" " + key + " ║ ".PadLeft(padLeft - key.Length) + lines[i].PadRight(padRight - 1));
+                }
+                else
+                {
+                    System.Console.Write("║".PadLeft(padLeft) + lines[i].PadRight(padRight));
+                }
+                System.Console.BackgroundColor = ConsoleColor.Black;
+                System.Console.WriteLine("║");
+            }
+        }
+
+        private static void WriteTableEntry(string key, string value)
+        {
+            currentColor = currentColor == ConsoleColor.Black ? ConsoleColor.DarkGray : ConsoleColor.Black;
+            string[] lines = value.Split('∞');
+            WriteTableLine(key, lines);
+        }
+
+        public static void WriteTable<T, U>(IList<Tuple<T, U>> list, string[] tableHeaders = null)
+        {
+            if (tableHeaders == null)
+                headers = new string[] { "Index", "Value" };
+            else
+                headers = tableHeaders;
+
+            padLeft = Math.Max(
+                        list.Max(x => x.Item1.ToString().Length),
+                        headers[0].Length)
+                        + 3;
+
+            padRight = Math.Min(Math.Max(
+                        list.Max(x => x.Item2.ToString().Length),
+                        headers[1].Length)
+                        + 2, 50);
+
+            string header = headers.Aggregate((a, b) => " ║ " + a + " ║ " + b + " ") +
+                "".PadRight(padRight - headers[1].Length - 2) + "║";
+
+            string topper = " ╔" + "╦".PadLeft(padLeft, '═') + "".PadRight(padRight, '═') + "╗" + Environment.NewLine + header;
+            string footer = " ╚" + "╩".PadLeft(padLeft, '═') + "".PadRight(padRight, '═') + "╝";
+
+            System.Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine(topper);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                string[] values = list[i].Item2.ToString().Split(' ');
+                int characterCount = 0;
+                for (int j = 0; j < values.Length; j++)
+                {
+                    characterCount += values[j].Length;
+                    if (characterCount >= 40 && !(values[0].Length > 40))
+                    {
+                        var tempList = values.ToList();
+                        tempList.Insert(j, "∞");
+                        values = tempList.ToArray();
+                        characterCount = 0;
+                    }
+                }
+                if (values[0].Length > 48)
+                {
+                    for (int j = 48; j < values[0].Length; j += 48)
+                    {
+                        var temp = values[0];
+                        temp = temp.Insert(j, "∞ ");
+                        values = new string[] { temp };
+                    }
+                }
+                string value = values.Aggregate((a, b) => a + " " + b);
+                WriteTableEntry(list[i].Item1.ToString(), value);
+            }
+            System.Console.WriteLine(footer);
+        }
+    }
+}
